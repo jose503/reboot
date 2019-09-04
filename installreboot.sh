21216b95bde41fa7affdee2318dabc94d2d2294d
jose503 <45978904+jose503@users.noreply.github.com>
2019-09-04 14:47:27 -0600

    Add files via upload

---
+++ installreboot.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo DEAMON OF REBOOT..........
+date
+sleep 2s
+mv reboot.py /home/pi/.reboot.py
+sudo mv reboot.service /etc/systemd/system/
+sudo systemctl enable rfcomm
+sudo systemctl start rfcomm
+exit
+
+
