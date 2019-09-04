6340bb7c246025e61c478038b5ca09aaf0b0ac31
jose503 <45978904+jose503@users.noreply.github.com>
2019-09-04 14:56:49 -0600

    Update installreboot.sh

--- installreboot.sh
+++ installreboot.sh
@@ -5,8 +5,8 @@ date
 sleep 2s
 mv reboot.py /home/pi/.reboot.py
 sudo mv reboot.service /etc/systemd/system/
-sudo systemctl enable rfcomm
-sudo systemctl start rfcomm
+sudo systemctl enable reboot.service
+sudo systemctl start reboot.service
 cd /home/pi/
 sudo rm -R reboot
 exit
